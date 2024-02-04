85aaba38c3ab0f392ff6794d07827c2e86a129be
Christoph Renzing <christoph.renzing@imf.tu-freiberg.de>
2024-02-04 14:16:51 +0100

    updated formulas

--- pyroll/lee_profile_model/roll_pass.py
+++ pyroll/lee_profile_model/roll_pass.py
@@ -9,29 +9,34 @@ def bulge_radius_round_oval(self: RollPass.OutProfile, cycle: bool):
     if cycle:
         return None
 
-    if "round" in self.roll_pass.in_profile.classifiers and "oval" in self.roll_pass.classifiers:
-        rp = self.roll_pass
-        weight = (rp.roll.groove.usable_width - self.width) / (
-                    rp.roll.groove.usable_width - rp.in_profile.width)
+    rp = self.roll_pass
 
-        usable_radius = (rp.roll.groove.r2 * rp.height - 1 / 4 * (self.width ** 2 + rp.height ** 2)) / (
-                2 * rp.roll.groove.r2 - rp.roll.groove.usable_width)
-
-        return rp.in_profile.equvalent_radius * weight + usable_radius * (1 - weight)
+    if "round" in rp.in_profile.classifiers and "oval" in rp.classifiers:
+        weight = (rp.roll.groove.usable_width - self.width) / (rp.roll.groove.usable_width - rp.in_profile.width)
+        usable_radius = (rp.roll.groove.r2 * rp.height - 1 / 4 * (
+                rp.roll.groove.usable_width ** 2 + rp.height ** 2)) / (
+                                2 * rp.roll.groove.r2 - rp.roll.groove.usable_width)
+        bulge_radius = rp.in_profile.equvalent_radius * weight + usable_radius * (1 - weight)
+        return bulge_radius
 
 
 @RollPass.OutProfile.bulge_radius
 def bulge_radius_oval_round(self: RollPass.OutProfile, cycle: bool):
     if cycle:
         return None
+    rp = self.roll_pass
 
-    if "oval" in self.roll_pass.in_profile.classifiers and "round" in self.roll_pass.classifiers:
-        rp = self.roll_pass
+    if "oval" in rp.in_profile.classifiers and "round" in rp.classifiers:
+
         oval_radius = rp.prev_of(RollPass).roll.groove.r2
+        weight = (rp.roll.groove.usable_width - self.width) / (rp.roll.groove.usable_width - rp.in_profile.width)
 
-        weight = (2 * rp.roll.groove.r2 - self.width) / (2 * rp.roll.groove.r2 - rp.in_profile.width)
+        if rp.height == 2 * rp.roll.groove.r2:
+            usable_radius = 2 * rp.roll.groove.r2
+        else:
+            usable_radius = rp.roll.groove.r2 + (rp.height - 2 * rp.roll.groove.r2)
 
-        return oval_radius * weight + rp.roll.groove.r2 * (1 - weight)
+        return oval_radius * weight + usable_radius * (1 - weight)
 
 
 @RollPass.OutProfile.cross_section
@@ -41,9 +46,9 @@ def cross_section(self: RollPass.OutProfile, cycle: bool):
 
     cs = self.cross_section
 
-    if self.has_set_or_cached("bulge_radius"):
+    if self.has_value("bulge_radius"):
         circle_center = self.width / 2 - self.bulge_radius
-        p = Point(circle_center, 0).buffer(self.bulge_radius)
-
-        return unary_union([cs, p])
+        left_circle = Point(circle_center, 0).buffer(self.bulge_radius)
+        right_circle = Point(-circle_center, 0).buffer(self.bulge_radius)
+        return unary_union([left_circle, cs, right_circle])
     return cs
